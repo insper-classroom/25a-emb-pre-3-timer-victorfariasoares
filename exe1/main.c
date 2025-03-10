98a4e0144f733c8cc9f91377d9c64db8ac2c63be
victor <victorfs2@al.insper.edu.br>
2025-03-10 09:31:49 -0300

    ex1 feito

--- exe1/main.c
+++ exe1/main.c
@@ -5,7 +5,11 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
+volatile int g_timer_0 = 0;
 volatile int flag_f_r = 0;
+volatile int timer_rodando = 0;
+repeating_timer_t timer_0;
+
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -14,10 +18,16 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true; // keep repeating
+}
+
 int main() {
     stdio_init_all();
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -25,11 +35,34 @@ int main() {
 
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
+    
+    int timer_0_hz = 2;
 
     while (true) {
 
         if (flag_f_r) {
             flag_f_r = 0;
+            if (!timer_rodando){
+                if (!add_repeating_timer_us(1000000 / timer_0_hz, 
+                    timer_0_callback,
+                    NULL, 
+                    &timer_0)){
+                        printf("Failed to add timer\n");
+                    } else {
+                        timer_rodando = 1;
+                        printf("Hello Timer 0\n");
+                    }
+            } else {
+                cancel_repeating_timer(&timer_0);
+                timer_rodando = 0;
+                gpio_put(LED_PIN_R, 0);
+                printf("Timer cancelado\n");
+            }
         }
+        if (g_timer_0) {
+            g_timer_0 = 0;
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+        }
+        sleep_ms(50);
     }
 }
